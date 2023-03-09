1ec0df3abbf38dd81ed5855696754dd08c8dc733
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-09 16:47:33 +0900

    3/9 game

--- study/src/App.js
+++ study/src/App.js
@@ -3,66 +3,70 @@ import { useState } from 'react';
 import './App.css';
 
 const choice = {
-  rock : {
-    name : "Rock",
-    img : "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-300x300.jpg",
+  rock: {
+    name: "Rock",
+    img: "https://media.istockphoto.com/photos/stone-pebble-gray-picture-id1288973456?b=1&k=20&m=1288973456&s=170667a&w=0&h=GBGgp4yrZv4ooDBws8yHF24sJ3rkEpObYsBWpVNKFT8=",
   },
   scissors: {
-    name : "Scissors",
-    img : "https://masflex.com.ph/wp-content/uploads/2020/09/HI-426.png",
+    name: "Scissors",
+    img: "https://www.ikea.com/kr/en/images/products/sy-scissors__0112301_pe263788_s5.jpg?f=s",
   },
-  paper : {
-    name : "Paper",
-    img : "https://www.collinsdictionary.com/images/full/paper_111691001.jpg",
+  paper: {
+    name: "Paper",
+    img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg",
   },
-}
-
+};
 function App() {
-  // 가위바위보 게임
-  // 1. 박스 2개 (타이틀,사진,결과)
-  // 2. 가위바위보 버튼
-  // 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-  // 4. 컴퓨터는 랜덤하게 아이템 선택이 됨
-  // 5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따짐
-  // 6. 승패의 결과에 따라 테두리 색이 바뀐다(이기면 - 초록, 지면- 빨강, 비기면 - 검은색)
-
-  // 인자값으로 버튼을 구분시킴 
-  // 리액트에서 플레이 함수를 () 쓰면 즉시 실행시키기 때문애 콜백함수로 넣어줘야함
-  // UI 화면이 보여지자 마자 버튼을 클릭하지 않았는데도 실행되는 것을 방지
-
-  const [userSelect, setUserSelect]= useState(null);
+  const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
-
+  const [result, setResult] = useState("");
   const play = (userChoice) => {
-    setUserSelect(choice[userChoice])
-    let computerChoice = randomChoice()
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
   };
-  const randomChoice = () => {
-    let itemArray = Object.keys(choice); // 객체의 키값을 배열값으로 바꿔줌
-    console.log(itemArray)
-    // floor 소수점자리를 버림
-    let randomItem = Math.floor(Math.random()* itemArray.length);
-    let final = itemArray[randomItem]
-    return choice[final];
-  }
 
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수다
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value", randomItem);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    // user == computer tie
+    // user == rock, computer == "scissors" user 이긴거지
+    // user == "rock" computer == paper   user 진거지
+    // user == scissors computer paper    user 이긴거지
+    // user == scissors computer rock     user 진거지
+    // user == paper computer rock   user 이긴거지
+    // user paper computer scissors user 진거지
+
+    if (user.name == computer.name) {
+      return "tie";
+    } else if (user.name == "Rock")
+      return computer.name == "Scissors" ? "win" : "lose";
+    else if (user.name == "Scissors")
+      return computer.name == "Paper" ? "win" : "lose";
+    else if (user.name == "Paper")
+      return computer.name == "Rock" ? "win" : "lose";
+  };
   return (
     <div>
-      <div className='main'>
-      <Box title="You" item={userSelect} />
-      <Box title="Computer" item={computerSelect} />
-      </div> 
-      <div className='main'>
-        <button onClick={()=>play("scissors")} type="">가위</button>
-        <button onClick={()=>play("rock") } type="">바위</button>
-        <button onClick={()=>play("paper")} type="">보</button>
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
-
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
-    
-    
-    
   );
 }
 
