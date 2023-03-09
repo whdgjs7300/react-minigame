e7d74fa83d678f4c15ad0b3fb5619a6712cd7cde
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-09 15:17:52 +0900

    3/9 miniproject

--- study/src/App.js
+++ study/src/App.js
@@ -3,19 +3,18 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const increase = () => {
-    setCounter(counter + 1);
-    
-  }
+  // 가위바위보 게임
+  // 1. 박스 2개 (타이틀,사진,결과)
+  // 2. 가위바위보 버튼
+  // 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+  // 4. 컴퓨터는 랜덤하게 아이템 선택이 됨
+  // 5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따짐
+  // 6. 승패의 결과에 따라 테두리 색이 바뀐다(이기면 - 초록, 지면- 빨강, 비기면 - 검은색)
 
   return (
     <div>
       
-      <div>
-        {counter}
-      </div>
-      <button onClick={increase}>클릭</button>
+      
 
       
 
