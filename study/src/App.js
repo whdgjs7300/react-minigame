4eda5cac1e49d00c4180769d0ecc2d1be825bfd7
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-11 22:51:06 +0900

    3/11 set

--- study/src/App.js
+++ study/src/App.js
@@ -20,6 +20,8 @@ function App() {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
   const [result, setResult] = useState("");
+
+  
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
